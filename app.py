8ecda853f6bb60921a780b32e5d65f1bd578841f
yvkrishnapraneeth <147371782+yvkrishnapraneeth@users.noreply.github.com>
2024-03-08 16:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,28 @@ import numpy as np
 app=Flask(__name__)
 camera = cv2.VideoCapture(0)
 # Load a sample picture and learn how to recognize it.
-krish_image = face_recognition.load_image_file("Krish/krish.jpg")
-krish_face_encoding = face_recognition.face_encodings(krish_image)[0]
+abhijith_image = face_recognition.load_image_file("abhijith/abhijith.jpg")
+abhijith_face_encoding = face_recognition.face_encodings(abhijith_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-bradley_image = face_recognition.load_image_file("Bradley/bradley.jpg")
-bradley_face_encoding = face_recognition.face_encodings(bradley_image)[0]
+ashbar_image = face_recognition.load_image_file("ashbar/ashbar.jpg")
+ashbar_face_encoding = face_recognition.face_encodings(ashbar_image)[0]
+
+# Load a third sample picture and learn how to recognize it.
+rizan_image = face_recognition.load_image_file("rizan/rizan.jpg")
+rizan_face_encoding = face_recognition.face_encodings(rizan_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    krish_face_encoding,
-    bradley_face_encoding
+    abhijith_face_encoding,
+    ashbar_face_encoding,
+    rizan_face_encoding 
 ]
 known_face_names = [
-    "Krish",
-    "Bradly"
+    "abhijith",
+    "ashbar",
+    "rizan"
+    
 ]
 # Initialize some variables
 face_locations = []
